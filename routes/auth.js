6b3167eec621d8f0c859d041393569e52f6d13ca
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-12-18 21:39:17 -0700

    Fix linting issues on all files
    ESLint complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
+const express = require('express');
+const debug = require('debug');
+
+const router = express.Router();
+const passport = require('passport');
 
 // Perform the login, after login Auth0 will redirect to callback
 router.get('/login', passport.authenticate('auth0', {
-  scope: 'openid email profile'
-}), function (req, res) {
+  scope: 'openid email profile',
+}), (req, res) => {
   res.redirect('/');
 });
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
-router.get('/callback', function (req, res, next) {
-  passport.authenticate('auth0', function (err, user, info) {
+router.get('/callback', (req, res, next) => {
+  passport.authenticate('auth0', (err, user, info) => {
+    debug(info);
     if (err) { return next(err); }
     if (!user) { return res.redirect('/login'); }
-    req.logIn(user, function (err) {
-      if (err) { return next(err); }
-      const returnTo = req.session.returnTo;
+    return req.logIn(user, (loginErr) => {
+      if (loginErr) { return next(loginErr); }
+      const { returnTo } = req.session;
       delete req.session.returnTo;
-      res.redirect(returnTo || '/user');
+      return res.redirect(returnTo || '/user');
     });
   })(req, res, next);
 });
