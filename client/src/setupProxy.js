8fc9dc88399fa93abd2357a76493c496f97160e4
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-01-07 00:41:24 -0700

    Add the auctions route to the proxy list

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
   app.use(proxy('/login', { target: 'http://localhost:3001/' }));
   app.use(proxy('/callback', { target: 'http://localhost:3001/' }));
   app.use(proxy('/logout', { target: 'http://localhost:3001/' }));
+  app.use(proxy('/auctions', { target: 'http://localhost:3001/' }));
 };
