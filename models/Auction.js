b6bf5137531341260e297c861dd2eb3cb3b1f8e8
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-01-11 21:35:37 -0700

    Updated Schema

--- models/Auction.js
+++ models/Auction.js
@@ -2,25 +2,36 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AuctionSchema = new Schema({
-    id:{
-        type: String,
-        required: true
-    },
-    userID:{
-        type: String,
-        required: true
-    },
-    title:{
-        type: String,
-        required: true
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    images:{
-        type:[String]
-    }
+  id: {
+    type: String,
+    required: true,
+  },
+  userID: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  startingDate: {
+    type: Date,
+    required: true,
+  },
+  endOfAuction: {
+    type: Date,
+    required: true,
+  },
+  images: {
+    type: [String],
+  },
+  minimumBid: {
+    type: Number,
+  },
 });
 
 // Init the User model schema
---
+++ test/UsersModel.test.js
@@ -0,0 +1,20 @@
+const db = require('./models/Auction');
+// const sum = require('./sum');
+
+const MONGODB_URI = 'mongodb://localhost/easyAuctionTest';
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+
+
+test('Verify that Mongo is connected to the test suite', async () => {
+  await mongoose.connect(MONGODB_URI, options).then(
+    async () => {expect(err).toBeFalsy();}
+  );
+});
+
+
+test('Dummy Test', () => {
+  expect(1).toBe(1);
+});
