8298d2eaa824040a56c8bc9135a387313e6a12c3
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-01-30 00:51:48 -0700

    Fix static path

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 app.use(flash());
