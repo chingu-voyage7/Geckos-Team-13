81874e0b812024c7ba9976f7ea0e07d3dd1a377b
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-02-06 23:50:12 -0700

    Add bidding endpoint

--- routes/auction.js
+++ routes/auction.js
@@ -11,8 +11,29 @@ const router = express.Router();
 router.get('/auction', (req, res, next) => {
   try {
     const { id } = req.query;
-    db.Auction.find(id).then((dbData) => {
-      res.json({ data: dbData });
+    db.Auction.findOne(id).then((auctionItem) => {
+      res.json({ data: auctionItem });
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('auction/:id/bid', (req, res, next) => {
+  try {
+    const { bidAmount } = req.body;
+    const { id } = req.param.id;
+    db.Auction.findOne(id).then((auctionItem) => {
+      debug(auctionItem);
+      if (bidAmount > auctionItem[0].currentBid) {
+        db.Auction.update(id, { currentBid: bidAmount }, (err, doc) => {
+          if (err) {
+            next(err);
+          } else {
+            res.json(doc);
+          }
+        });
+      }
     });
   } catch (err) {
     next(err);
